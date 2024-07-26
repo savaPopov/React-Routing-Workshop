c652b3c8911df9d07e5681e8a89f545361f5d51f
Sava Popov <157402590+savaPopov@users.noreply.github.com>
2024-07-26 14:00:13 +0300

    Routing and Navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,11 @@
 import Header from "./components/header/Header"
+
+import { Routes, Route } from "react-router-dom"
 import Home from "./components/home/Home"
+import Login from "./components/login/Login"
+import Register from "./components/register/Register"
+import Create from "./components/create/Create"
+import Catalog from "./components/catalog/Catalog"
 
 
 
@@ -10,8 +16,15 @@ function App() {
     <>
       <div id="box">
         <Header />
+
         <main id="main-content">
-          <Home/>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/catalog" element={<Catalog />} />
+          </Routes>
         </main>
       </div>
 
